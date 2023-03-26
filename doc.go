d560ef4212e31117b3d10106854146e23d63c6ca
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2023-03-26 01:24:13 -0600

    generate an API client

---
+++ doc.go
@@ -0,0 +1,11 @@
+// doc.go
+//
+// Copyright (c) 2023 Junpei Kawamoto
+//
+// This software is released under the MIT License.
+//
+// http://opensource.org/licenses/mit-license.php
+
+package civitai
+
+//go:generate go run github.com/go-swagger/go-swagger/cmd/swagger@v0.30.4 generate client -f ./openapi.yaml -t .
